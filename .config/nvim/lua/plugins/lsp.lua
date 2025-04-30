110b50313763ebb939a464beafa2461430db99cc
Lnxy-0 <5903123079@email.ncu.edu.cn>
2025-04-30 21:55:09 +0800

    feat: add lsp

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+
+	-- example calling setup directly for each LSP
+	config = function()
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local lspconfig = require("lspconfig")
+
+		lspconfig["lua_ls"].setup({ capabilities = capabilities })
+        lspconfig["pyright"].setup({capabilities = capabilities})
+	end,
+}
